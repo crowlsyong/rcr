874c29133a47625856cc2bc5d60b9ec649a88bbe
crowlsyong <81304661+crowlsyong@users.noreply.github.com>
2025-04-24 20:00:00 -0500

    Update 404 page
    dark theme!

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -6,20 +6,20 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+      <div class="min-h-screen bg-[#0F1729] text-white flex items-center justify-center">
+        <div class="text-center px-4 py-8">
           <img
             class="my-6"
             src="/risk.png"
             width="128"
             height="128"
-            alt="the RISK mascott: a raccoon"
+            alt="the RISK mascot: a raccoon"
           />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
-            The page you were looking for doesn't exist.
-          </p>
-          <a href="/" class="underline">Go back home</a>
+          <h1 class="text-4xl font-bold mb-4">404 - Page not found</h1>
+          <p class="my-4">The page you were looking for doesn't exist.</p>
+          <a href="/" class="underline text-green-400 hover:text-green-500">
+            Go back home
+          </a>
         </div>
       </div>
     </>
