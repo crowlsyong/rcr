653f199cd2be31357cb9b8e90d52d0046c8a4ac5
crowlsyong <81304661+crowlsyong@users.noreply.github.com>
2025-05-09 07:35:34 -0500

    deno fmt

--- routes/api/score.ts
+++ routes/api/score.ts
@@ -88,22 +88,22 @@ function computeMMR(
   const rankWeight = Math.max(0, Math.min(1, 1 - (rank - 1) / (maxRank - 1)));
   const rankMMR = rankWeight * 1000;
 
-let transactionMMR = 0;
+  let transactionMMR = 0;
 
-if (transactionCount < 5) {
-  transactionMMR = -1000000;
-} else if (transactionCount <= 20) {
-  // From -100,000 to -10,000
-  transactionMMR = -100000 + ((transactionCount / 10) * 9000);
-} else if (transactionCount <= 100) {
-  // From -10,000 to 0
-  transactionMMR = -10000 + (((transactionCount - 20) / 80) * 10000);
-} else if (transactionCount <= 1000) {
-  // From 0 to 1000, linearly over 900 txns
-  transactionMMR = ((transactionCount - 100) / 900) * 1000;
-} else {
-  transactionMMR = 1000;
-}
+  if (transactionCount < 5) {
+    transactionMMR = -1000000;
+  } else if (transactionCount <= 20) {
+    // From -100,000 to -10,000
+    transactionMMR = -100000 + ((transactionCount / 10) * 9000);
+  } else if (transactionCount <= 100) {
+    // From -10,000 to 0
+    transactionMMR = -10000 + (((transactionCount - 20) / 80) * 10000);
+  } else if (transactionCount <= 1000) {
+    // From 0 to 1000, linearly over 900 txns
+    transactionMMR = ((transactionCount - 100) / 900) * 1000;
+  } else {
+    transactionMMR = 1000;
+  }
 
   // The Credit Score Equation
   return ((balance * 0.15) + (ageDays * 0.1) + (totalMana * 0.5)) + rankMMR +
