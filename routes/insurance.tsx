648efa251d7a42cbb7f984e286f59a385702fbd2
crowlsyong <81304661+crowlsyong@users.noreply.github.com>
2025-05-09 00:35:08 -0500

    try again to fix spacing

--- routes/insurance.tsx
+++ routes/insurance.tsx
@@ -4,7 +4,7 @@ import InsuranceCalc from "../islands/InsuranceCalc.tsx";
 export default function Home() {
   return (
     /*<PasswordGate> */
-    <div class="min-h-screen pt-14  bg-[#0F1729] text-white dark:bg-[#0F1729] dark:text-white">
+    <div class="pt-14  bg-[#0F1729] text-white dark:bg-[#0F1729] dark:text-white">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center px-4 py-8 md:pt-8">
         <h1 class="text-xl sm:text-4xl font-bold text-center mb-4">
           🦝Insurance Calculator
