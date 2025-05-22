9f66c48b32175bf874fb820bb01857f3bc528d74
crowlsyong <81304661+crowlsyong@users.noreply.github.com>
2025-05-22 11:49:51 -0500

    Migrate CSP back to routes + add version text

---
+++ components/Version.tsx
@@ -0,0 +1,21 @@
+export default function Version() {
+  return (
+    <div class="p-4 text-center text-sm text-gray-600">
+      <div class="flex justify-center items-center gap-2 text-xs text-gray-500">
+        <span>
+          Built for{" "}
+          <a
+            href="https://manifold.markets/"
+            class="underline hover:text-blue-500"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            manifold.markets
+          </a>
+        </span>
+        <span class="text-gray-400">|</span>
+        <span class="text-gray-500 text-xs">Version 2.0.2</span>
+      </div>
+    </div>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,26 +1,33 @@
+import Version from "../components/Version.tsx";
 import CreditScore from "../islands/CreditScore.tsx";
-import { RouteContext } from "$fresh/server.ts";
+import { useCSP } from "$fresh/runtime.ts";
+import { RouteConfig, RouteContext } from "$fresh/server.ts";
 
 export default function Home(_req: Request, _ctx: RouteContext) {
+  useCSP((csp) => {
+    // Setting the allowed sources for content
+    csp.directives.defaultSrc = ["'self'"];
+    csp.directives.scriptSrc = ["'self'", "https://manifold.markets"];
+    csp.directives.styleSrc = ["'self'", "https://manifold.markets"];
+    csp.directives.imgSrc = [
+      "'self'",
+      "https://firebasestorage.googleapis.com",
+      "https://lh3.googleusercontent.com",
+    ];
+    csp.directives.fontSrc = ["'self'", "https://manifold.markets"];
+  });
   return (
     <div class="pt-14 bg-[#0F1729] text-white dark:bg-[#0F1729] dark:text-white">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center px-4 py-8 md:pt-8">
         <h1 class="text-xl sm:text-4xl font-bold text-center mb-4">
           🦝 Credit Score
         </h1>
-        <p class="text-sm text-center mb-4 text-gray-500">
-          Built for{" "}
-          <a
-            href="https://manifold.markets/"
-            class="underline hover:text-blue-500"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            manifold.markets
-          </a>
-        </p>
+        <Version />
         <CreditScore />
       </div>
     </div>
   );
 }
+export const config: RouteConfig = {
+  csp: true,
+};
