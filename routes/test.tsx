015844443b44c683d748e578336a2403f4acfb7c
crowlsyong <81304661+crowlsyong@users.noreply.github.com>
2026-01-30 15:10:12 -0600

    test

--- routes/test.tsx
+++ routes/test.tsx
@@ -7,7 +7,7 @@ export default function Home(_req: Request, _ctx: RouteContext) {
     <div class="pt-14 bg-[#0F1729] text-white dark:bg-[#0F1729] dark:text-white">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center px-4 py-8 md:pt-8">
         <h1 class="text-xl sm:text-4xl font-bold text-center mb-4">
-          🦝 Test Page
+          🦝 Test Page test
         </h1>
       </div>
     </div>
